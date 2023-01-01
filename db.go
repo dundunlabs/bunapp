9e532c3d5d2e5020eec0cc8266b60b28370af336
Dung Tran <dung28.td@gmail.com>
2023-01-01 16:09:21 +0700

    Add AutoMigrate method

--- db.go
+++ db.go
@@ -1,6 +1,12 @@
 package bunapp
 
-import "github.com/uptrace/bun"
+import (
+	"context"
+	"log"
+
+	"github.com/uptrace/bun"
+	"github.com/uptrace/bun/migrate"
+)
 
 type DB struct {
 	*bun.DB
@@ -8,4 +14,28 @@ type DB struct {
 
 func newDB(db *bun.DB) *DB {
 	return &DB{db}
+}
+
+func (db *DB) AutoMigrate(ctx context.Context, migrations *migrate.Migrations) error {
+	migrator := migrate.NewMigrator(db.DB, migrations)
+
+	if err := migrator.Init(ctx); err != nil {
+		return err
+	}
+
+	if err := migrator.Lock(ctx); err != nil {
+		return err
+	}
+	defer migrator.Unlock(ctx)
+
+	group, err := migrator.Migrate(ctx)
+	if err != nil {
+		return err
+	}
+	if group.IsZero() {
+		log.Println("there are no new migrations to run (database is up to date)")
+		return nil
+	}
+	log.Println("migrated to", group)
+	return nil
 }
