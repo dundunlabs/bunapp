3e912ca5346c12c97b76341bdd10bd3414434363
Dung Tran <dung28.td@gmail.com>
2023-01-02 15:17:40 +0700

    BindQuery method

---
+++ router/router.go
@@ -0,0 +1,25 @@
+package router
+
+import (
+	"encoding/json"
+
+	"github.com/uptrace/bunrouter"
+)
+
+func BindQuery(req bunrouter.Request, v any) error {
+	query := make(map[string]any)
+	for k, v := range req.URL.Query() {
+		if len(v) == 1 {
+			query[k] = v[0]
+		} else {
+			query[k] = v
+		}
+	}
+
+	b, err := json.Marshal(query)
+	if err != nil {
+		return err
+	}
+
+	return json.Unmarshal(b, v)
+}
